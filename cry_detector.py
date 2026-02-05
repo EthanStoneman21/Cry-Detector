9217b969995fd1d2dc28e527b064f927b64cea55
EthanStoneman21 <estoneman23@icloud.com>
2026-02-05 16:32:31 -0500

    Working model and live audio pick up working. LETS GOOOOO

--- cry_detector.py
+++ cry_detector.py
@@ -1,39 +1,76 @@
 import RPi.GPIO as GPIO
 import numpy as np
+import pyaudio as pa
 import librosa as lr
-import pyaudio as ad
 from tflite_runtime.interpreter import Interpreter
 
-# preprocessing function, pyaudio grants raw audio,, need a NumPy array
+def record():
+    CHUNK = 1024
+    FORMAT = pa.paInt16
+    CHANNELS = 1
+    RATE = 16000
+
+    p = pa.PyAudio()
+
+    stream = p.open(format = FORMAT,
+                    channels = CHANNELS,
+                    rate = RATE,
+                    input = True,
+                    frames_per_buffer = CHUNK)
+    print("Recording...")
+
+    frames = []
+    seconds = 6
+    for i in range(0, int(RATE / CHUNK * seconds)):
+        data = stream.read(CHUNK)
+        num_arr = np.frombuffer(data, dtype = np.int16)
+        frames.append(num_arr)
+
+    print("Recording stopped")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    return np.concatenate(frames) #convert to numpy
+
+# preprocessing function, pyaudio grants raw audio, need a NumPy array, take preprocessing function from training and convert to numpy for tflite
 def preprocess(audio): 
+    #convert audio to float32
+    audio = audio.astype(np.float32)
+
     #model expects a flat shape
     audio = audio.flatten()
 
     #normalize fix for bad spectrograms, helps make the amplitude consistent
-    audio = audio / (np.reduce_max(np.abs(audio)) + 1e-6)
+    audio = audio / (np.max(np.abs(audio)) + 1e-6)
 
     #trim or pad to 6 seconds
     audio = audio[:96000]
 
     if len(audio) < 96000:
         #padding for shorter clips
-        zero_padding = np.zeros(96000 - len(audio), dtype=np.float32)
-        audio = np.concatenate([zero_padding, audio],0)
+        audio = np.concatenate([audio, np.zeros(96000 - len(audio), dtype = np.float32)])
 
-    #converts waveform into 20ms windows with 2ms hop using librosa
-    spectrogram = tf.signal.stft(wav, frame_length=320, frame_step=32)
+    #converts waveform into 20ms windows with 2ms hop
+    spectrogram = lr.stft(audio, n_fft = 512, hop_length = 32, center = True, pad_mode = "reflect")
 
     #drop phase and keep magnitude
     spectrogram = np.abs(spectrogram)
 
     #log scaling helps noise become visible in the spectrograms especially the quiet clips
-    spectrogram = np.math.log(spectrogram + 1e-6)
+    spectrogram = np.log(spectrogram + 1e-6)
+
+    spectrogram = spectrogram.T
+    spectrogram = spectrogram[:2991, :257]
 
     #channel dimension for CNN (convolutional neural network)
-    spectrogram = np.expand_dims(spectrogram, axis=2) #set channel, 3D tensor
-    spectrogram = np.expand_dims(spectrogram, axis=0) #set batch, 4D tensor
+    spectrogram = np.expand_dims(spectrogram, axis = 2) #set channel, 3D tensor
+    spectrogram = np.expand_dims(spectrogram, axis = 0) #set batch, 4D tensor
     
     return spectrogram.astype(np.float32)
+
+audio = record()
 
 #Load TFlite model and allocate tensors
 interpreter = Interpreter(model_path="cry_detector.tflite")
@@ -53,4 +90,3 @@ interpreter.invoke()
 output_data = interpreter.get_tensor(output_details[0]['index'])
 print(output_data)
 
-input
